b9aeb0cc8b7e654e90c48dd4706a430f44eff512
Angelo Paolo Obispo <angelopaolo.obispo@gmail.com>
2016-03-14 09:52:09 +0800

    editing the scaffold exercise - edited params method in ticket controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -69,6 +69,6 @@ class TicketsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ticket_params
-      params.require(:ticket).permit(:ticket_name, :seat_seq_id, :address, :price, :email_address)
+      params.require(:ticket).permit(:ticket_name, :seat_seq_id, :address, :price, :email_address, :phone)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109022353) do
+ActiveRecord::Schema.define(version: 20160314012901) do
 
   create_table "tickets", force: :cascade do |t|
     t.string   "ticket_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160109022353) do
     t.string   "email_address"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "phone"
   end
 
 end
